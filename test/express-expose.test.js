8a37be64bfcbbcc22b00c2411f08e844da4a3dc9
Tj Holowaychuk <tj@vision-media.ca>
2011-04-07 16:52:54 -0700

    refactored buffering

--- test/express-expose.test.js
+++ test/express-expose.test.js
@@ -13,23 +13,6 @@ module.exports = {
     expose.version.should.match(/^\d+\.\d+\.\d+$/);
   },
   
-  'test app.expose(obj)': function(){
-    var app = express.createServer();
-    app.set('title', 'My Site');
-    app.set('default language', 'en');
-    app.expose(app.settings);
-    app.js.javascript.should.have.length(1);
-  },
-  
-  'test app.expose(obj, namespace, name)': function(){
-    var app = express.createServer();
-    app.set('title', 'My Site');
-    app.set('default language', 'en');
-    app.expose(app.settings, 'express.settings', 'settings');
-    app.expose({ foo: 'bar' }, 'express.utils', 'settings');
-    app.js.settings.should.have.length(2);
-  },
-  
   'test app.expose(name)': function(){
     var app = express.createServer();
     app.expose({ one: 1, two: 2, three: 3 });
