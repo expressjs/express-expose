1fa1ac73cbd2ab8e767834cc66d2f3708e7887d2
Tj Holowaychuk <tj@vision-media.ca>
2011-04-07 15:31:30 -0700

    implemented most of the lib

--- test/express-expose.test.js
+++ test/express-expose.test.js
@@ -5,10 +5,36 @@
 
 var express = require('express')
   , expose = require('express-expose')
-  , should = require('should');
+  , should = require('should')
+  , vm = require('vm');
 
 module.exports = {
   'test .version': function(){
     expose.version.should.match(/^\d+\.\d+\.\d+$/);
+  },
+  
+  'test app.expose(obj)': function(){
+    var app = express.createServer();
+    app.set('title', 'My Site');
+    app.set('default language', 'en');
+    app.expose(app.settings);
+    app.js.should.have.length(1);
+  },
+  
+  'test app.expose()': function(){
+    var app = express.createServer();
+    app.expose({ one: 1, two: 2, three: 3 });
+    app.expose({ title: 'My Site' }, 'express.settings');
+    app.expose({ add: function(a, b){ return a + b; } }, 'utils');
+    var js = app.expose()
+      , scope = {};
+
+    vm.runInNewContext(js, scope);
+    scope.express.one.should.equal(1);
+    scope.express.two.should.equal(2);
+    scope.express.three.should.equal(3);
+
+    scope.express.settings.title.should.equal('My Site');
+    scope.utils.add(1,5).should.equal(6);
   }
 };
