bff3cb2dcd799e1c3eae52e9d214f5ffa7ab8689
PG <pgherveou@gmail.com>
2012-08-08 02:15:11 +0200

    add req.expose test

--- test/express-expose.test.js
+++ test/express-expose.test.js
@@ -7,6 +7,7 @@ var express = require('express')
   , expose = require('../')
   , assert = require('assert')
   , should = require('should')
+  , request = require('supertest')
   , vm = require('vm');
 
 module.exports = {
@@ -132,5 +133,25 @@ module.exports = {
     vm.runInNewContext(js, scope);
     scope.sub(8,7).should.equal(1);
     scope.should.not.have.property('add');
-  }
+  },
+
+  'test req.expose': function (done) {
+      var app = express();
+      app.set('view engine', 'jade');
+      app.set('views', __dirname + '/views');
+      app.expose('var app = 1;');
+
+      app.get('/', function(req, res) {
+        res.expose('var res = 1;');
+        res.render('index');
+      });
+
+      request(app)
+        .get('/')
+        .end(function(err, res){
+          if (err) throw err;
+          res.text.should.match(/var res/);
+          done();
+        });
+    }
 };
